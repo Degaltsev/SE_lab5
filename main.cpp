e8b95993fb1ebaa060c94d7b12ef050420556e8c
Degalcev Ivan <Ivan_Degalcev@mail.ru>
2022-05-24 21:55:11 +0400

    Added summing

--- main.cpp
+++ main.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main()
 {
+    int a,b;
+    cout << "Enter two integer numbers" << endl;
+    cin >> a >> b;
+    int c;
+    c = a+b;
+    cout << " a+b= " << c << endl;
+
 }
